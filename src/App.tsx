fb38a7313382232dac7bad33210bf609711982df
Beksultan Dzhienbekov <bekadj14@gmail.com>
2023-11-14 17:43:41 +0600

    импортировал файл Alert и вывел алерты на страницу и добавил бутстрап стили

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import Modal from './Modal';
-
+import Alert from './Alert';
 
 const App: React.FC = () => {
     const [showModal, setShowModal] = useState(false);
-
+    const [showAlert, setShowAlert] = useState(true);
 
     const openModal = () => {
         setShowModal(true);
@@ -14,7 +14,6 @@ const App: React.FC = () => {
         setShowModal(false);
     };
 
-
     return (
         <div className="container mt-5">
             <button className="btn btn-primary" onClick={openModal}>
@@ -23,6 +22,19 @@ const App: React.FC = () => {
             <Modal show={showModal} onClose={closeModal} title="Some kinda modal title">
                 <p>This is modal content</p>
             </Modal>
+
+            {showAlert && (
+                <>
+                    <div className="mt-5">
+                        <Alert type="warning" onDismiss={closeAlert}>
+                              This is a warning type alert
+                        </Alert>
+                        <Alert type="success">This is a success type alert</Alert>
+                        <Alert type="primary">This is a primary type alert</Alert>
+                        <Alert type="danger">This is a danger type alert</Alert>
+                    </div>
+                </>
+            )}
         </div>
     );
 };
